37ff4db857a0184c89075c76c745a93d206d8d89
Sxitxma <169071982+Sxitxma@users.noreply.github.com>
2026-07-29 18:30:52 +0200

    added FORMATED_FILE_NAME (default = true)

--- src/il2cpp_dump.h
+++ src/il2cpp_dump.h
@@ -7,6 +7,10 @@
 
 #define MODULENAME "GameAssembly.dll"
 
+// true = dump-GAME_VERSION.cs
+// false = dump.cs
+#define FORMATED_FILE_NAME true
+
 void il2cpp_dump(void* handle, char* outDir, const char* il2cppModuleName);
 
 #endif //RIRU_IL2CPPDUMPER_IL2CPP_H
